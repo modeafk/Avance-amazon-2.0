2c32f3e072055c06fbce51906a32ae9bdfbc3799
modeafk <marcelo,torres.acuna@ucsp.edu.pe>
2022-05-12 22:21:16 -0500

    actualizando

--- static/toggleMenu.js
+++
@@ -1,9 +0,0 @@
-function toggleMenu() {
-    let menu = document.getElementById('toggle-menu');
-    if (menu.style.display == "") {
-        menu.style.display = "block";
-    }
-    else {
-        menu.style.display = "";
-    }
-}
